61bb7d573b19e0f436cebb75c9a00a0fab03b9f5
usama0011 <za5232208@gmail.com>
2021-10-15 08:47:30 +0500

    day one new build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Redirect } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import Tables from "./pages/Tables";
+import Maps from "./pages/Maps";
+import Footer from './components/Footer'
+
+// Tailwind CSS Style Sheet
+import "./assets/styles/tailwind.css";
+import Login from './components/Login'
+import Users from "./components/Users";
+import Analytics from "./components/Analytics";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <div className="md:ml-64">
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/settings" component={Settings} />
+          <Route exact path="/tables" component={Tables} />
+          <Route exact path="/maps" component={Maps} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/users" component={Users} />
+          <Route exact path="/analytics" component={Analytics} />
+          <Redirect from="*" to="/" />
+        </Switch>
+        <Footer />
+      </div>
+    </>
   );
 }
 
